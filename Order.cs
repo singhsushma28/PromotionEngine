d65f180edb9707d473c22041783a2e4b4c4e2c37
singhsushma28 <85681288+singhsushma28@users.noreply.github.com>
2021-06-11 13:04:18 +0530

    Create Order.cs

---
+++ Order.cs
@@ -0,0 +1,15 @@
+namespace PromotionEngine
+{
+  public class Order : IOrder
+  {
+    public Dictionary<string,int> GetOrderItems(Product products)
+    {
+    
+    }
+    
+    public int GetTotalOrderAmount(Dictionary<string,int> products)
+    {
+      
+    }
+  }
+}
