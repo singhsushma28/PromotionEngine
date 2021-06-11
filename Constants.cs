c4779229655c5c15aab5e8637133f56b76dbeb8e
singhsushma28 <85681288+singhsushma28@users.noreply.github.com>
2021-06-11 13:16:59 +0530

    Create Constants.cs

---
+++ Constants.cs
@@ -0,0 +1,13 @@
+namespace PromotionEngine
+{
+public class Constants
+{
+  public class ProductType
+  {
+    public const string ProductA="A";
+    public const string ProductB="B";
+    public const string ProductC="C";
+    public const string ProductD="D";
+  }
+ }
+}
