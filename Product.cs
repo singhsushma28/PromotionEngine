29e0d87f125b54b5c65a3295551583398214f617
singhsushma28 <85681288+singhsushma28@users.noreply.github.com>
2021-06-11 13:08:32 +0530

    Update Product.cs

--- Product.cs
+++ Product.cs
@@ -6,6 +6,13 @@ namespace PromotionEngine
         int UnitPrice {get;set;}
         public  List<Product> GetPriceForUnit()
         {
+          var list = new List<Product>();
+          list.Add(new Product{UnitId="A",UnitPrice=50});
+          list.Add(new Product{UnitId="B",UnitPrice=30});
+          list.Add(new Product{UnitId="C",UnitPrice=20});
+          list.Add(new Product{UnitId="D",UnitPrice=15});
+          
+          return list;
         }
   }
 
